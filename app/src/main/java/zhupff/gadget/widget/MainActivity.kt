03bf2ff0d54ad44f7773d579f4de650c3742189e
Zhupf <zhupff@qq.com>
2023-03-19 18:20:27 +0800

    Init project.

---
+++ app/src/main/java/zhupff/gadget/widget/MainActivity.kt
@@ -0,0 +1,10 @@
+package zhupff.gadget.widget
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
