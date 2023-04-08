c5052fa9c8326969d7ba312cd5a83abb7a983daf
Zhupf <zhupff@qq.com>
2023-04-08 21:54:53 +0800

    Add sample.

--- app/src/main/java/zhupff/gadget/widget/MainActivity.kt
+++ app/src/main/java/zhupff/gadget/widget/MainActivity.kt
@@ -1,10 +1,29 @@
 package zhupff.gadget.widget
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import zhupff.gadget.widget.attr.cornerClip
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(FrameLayout(
+            this, size = MATCH_PARENT to MATCH_PARENT
+        ) {
+            setBackgroundColor(Color.WHITE)
+            ImageViewX(
+                size = 400 to 400
+            ) {
+                frameLayoutParams {
+                    gravity = Gravity.CENTER
+                }
+                cornerClip {
+                    allCornerRadius = 200F
+                }
+                setImageResource(R.drawable.ic_launcher)
+            }
+        })
     }
 }
